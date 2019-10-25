7304584c9ad8c42aef2c14b54881fe3e0a931d83
Lucas <me@dotronglong.com>
2019-10-25 23:18:56 +0800

    Reformat example/lib/main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -28,29 +28,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final Spec spec = Spec(endpoints: {
-    "get_pet": {
-      "url": "{{api_url}}/{{version}}/pet/{{pet_id}}",
-      "method": "get"
-    }
-  }, parameters: {
-    "api_url": "https://petstore.swagger.io",
-    "version": "v2",
-  }, onSend: (Request request) {
-    request.set("start_time", DateTime.now().millisecondsSinceEpoch);
-  }, onReceive: (Request request, Response response) {
-    int startTime = request.get("start_time");
-    int endTime = DateTime.now().millisecondsSinceEpoch;
-    print("Request ${response.httpResponse.request.url.toString()} is completed in ${endTime - startTime} (ms)");
-  });
+  final Spec spec = Spec(
+      endpoints: {
+        "get_pet": {
+          "url": "{{api_url}}/{{version}}/pet/{{pet_id}}",
+          "method": "get"
+        }
+      },
+      parameters: {
+        "api_url": "https://petstore.swagger.io",
+        "version": "v2",
+      },
+      onSend: (Request request) {
+        request.set("start_time", DateTime.now().millisecondsSinceEpoch);
+      },
+      onReceive: (Request request, Response response) {
+        int startTime = request.get("start_time");
+        int endTime = DateTime.now().millisecondsSinceEpoch;
+        print(
+            "Request ${response.httpResponse.request.url.toString()} is completed in ${endTime - startTime} (ms)");
+      });
   Map<String, dynamic> pets = Map();
 
   @override
   void initState() {
     super.initState();
-    spec.call("get_pet", parameters: {
-      "pet_id": "3"
-    }).then((Response response) {
+    spec.call("get_pet", parameters: {"pet_id": "3"}).then((Response response) {
       if (response.statusCode == 200) {
         setState(() {
           pets = json.decode(utf8.decode(response.bodyBytes));
